337c0763dafab60248adeafc0308a446aaf1f8fc
Andrey Eferemov <samo.dur.andre@gmail.com>
2024-02-05 23:20:42 +0300

    feat: start project

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import { aos } from './modules/_aos.js'
+import { disabledBtnForm } from './modules/_disabledBtnForm.js'
+import { sendForm } from './modules/_form.js'
+import { hello } from './modules/_hello.js'
+import { initSlider } from './modules/_initSlider.js'
+import { pressDawn } from './modules/_pressDawn.js'
+import { pressUp } from './modules/_pressUp.js'
+
+hello()
+initSlider()
+aos()
+pressDawn()
+pressUp()
+disabledBtnForm()
+sendForm()
